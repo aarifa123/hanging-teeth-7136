076f69f9fb0a821995eb61363d423aad280b0198
jitender <trademetrader1137@gmail.com>
2023-01-18 23:01:24 +0530

    day-1-jitender

---
+++ scripts/Adminfile.js
@@ -0,0 +1,25 @@
+// POST DATA TO API LINK
+
+// OUR API LINK USE scriptURL AT PLACE OF URL TO FETCH DATA
+const scriptURL = "https://script.google.com/macros/s/AKfycbw5EDTQhh5rkeFjl5iHSahMK9gYJSZMKnMdRSlhVQGtqlNk0pUddO9_J87PjORnxNZvBw/exec";
+// POST DATA TO API LINK
+
+
+
+// Guys below part is only for admin side
+
+
+
+
+// DELETE DATA FROM API
+const deleteapilink = "https://v1.nocodeapi.com/vijayji11377/google_sheets/EOkGBiviWTVvfREe?tabId=sampleapi1&row_id=";
+// DELETE DATA FROM AP1
+
+// UPDATE DATA FROM API
+const updatedatafromapilink = "https://v1.nocodeapi.com/vijayji113777/google_sheets/SWrpTfizlJrmelxR?tabId=sampleapi1";
+// UPDATE DATA FROM API
+
+
+// ADMIN SIDE FETCH URL NOT FOR USER SIDE;
+const adminsidefetchurl = "https://script.googleusercontent.com/macros/echo?user_content_key=OWlD6cVUch3T0JJGlJZ8Sn_t22Ex-Tsua61e923ppcpk-XDSHfuN0UX_rewLfHiQunbgthpetFKaGFnC-3IXsgdGDDeVqxijm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHxcMLpTY64uk7KipC0tzgwA-CtFtpiqwclv_-BaugaHnTtJ-mDJacNtMEAPHEdJ_WVXoXee0IvfvEQ97MH3u0hyVWwnNcSLjtz9Jw9Md8uu&lib=M8J7kzd7Zlw2ThwXavx9Z1SZBtr-bOvlX";
+// ADMIN SIDE FETCH URL NOT FOR USER SIDE;
