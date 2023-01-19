9f5917840efd86c7f34749a3492b78ba2c5a04bb
aarifa123 <113410110+aarifa123@users.noreply.github.com>
2023-01-20 01:56:46 +0530

    Update signin.js

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,8 +1,46 @@
+
+// let username=document.getElementById("username")
+// let pass=document.getElementById("password")
+// let form=document.getElementById("signin-form")
+// form.addEventListener("submit",(e)=>{
+//     e.preventDefault()
+//     fetch("https://63c69db7dcdc478e15c55914.mockapi.io/UsersAPI")
+//     .then(res=>{
+//         return res.json()
+//     })
+
+//     .then(data=>{
+//         let user;
+//         for(let i=0;i<=data.length-1;i++){
+//             if(data[i].username===username.value&&data[i].password==pass.value){
+//                 console.log("true")
+//                 user=data[i]
+//                 break;
+//             }
+//             else{
+//                 alert ("UserID or password not match")
+//             }
+           
+//         }
+//         console.log(user)
+//        if(user.admin===true){
+//         window.location.href = "./admin.html";
+//        } 
+//        else if(user.admin===false){
+//         window.location.href = "./index.html";  
+//        }
+//     })
+
+
+
+// })
 let username=document.getElementById("username")
 let pass=document.getElementById("password")
-let form=document.getElementById("signin-form")
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
+let form=document.getElementById("signin")
+let loginbutton=document.getElementById("show-login")
+let Name=document.getElementById("name")
+form.addEventListener("click",()=>{
+    // e.preventDefault()
     fetch("https://63c69db7dcdc478e15c55914.mockapi.io/UsersAPI")
     .then(res=>{
         return res.json()
@@ -11,26 +49,39 @@ form.addEventListener("submit",(e)=>{
     .then(data=>{
         let user;
         for(let i=0;i<=data.length-1;i++){
-            if(data[i].username===username.value&&data[i].password==pass.value){
-                console.log("true")
+            if(data[i].username===username.value&&data[i].password==pass.value&&data[i].name==Name.value){
+            
                 user=data[i]
                 break;
             }
-            else{
-                alert ("UserID or password not match")
-            }
+            else if (username.value === "" || pass.value === ""||Name.value==="") {
+                alert("Please fill in the input fields.");
+              }
+              
            
         }
         console.log(user)
+        if(user){
+            localStorage.setItem("loginstatus", JSON.stringify({status: true, id: user.id}));
+            if(user.admin){
+                window.location.href = "admin.html";
+            }else{
+                window.location.href = "index.html";  
+            }
+        }
+        
        if(user.admin===true){
+localStorage.setItem("loginstatus", JSON.stringify({status: true, id: user.id}));
+
         window.location.href = "admin.html";
+       
        } 
        else if(user.admin===false){
+        localStorage.setItem("loginstatus", JSON.stringify({status: true, id: user.id}));
         window.location.href = "index.html";  
+        
        }
     })
 
 
-
 })
-
